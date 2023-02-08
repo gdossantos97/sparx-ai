cb47d2f990e5e2b66480363797f82c0bc0a1e14d
Gabriel <gabe.cs1997@gmail.com>
2023-02-08 12:41:22 -0500

    javascript

--- client/script.js
+++ client/script.js
@@ -19,3 +19,79 @@ function loader(element) {
     }
   }, 300)
 }
+
+function typeText (element, text) {
+  let index = 0;
+
+  let interval = setInterval (() => {
+    if (index < text.length) {
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16)
+
+
+  return `id${timestamp}-${hexadecimalString}`
+}
+
+function chatStripe (isAi, value, uniqueid) {
+  return (
+
+`    <div class="wrapper ${isAi && 'ai'}">
+      <div class="chat">
+        <div className='profile'>
+          <img src='${isAi ? bot : 'user'}'
+               alt='${isAi ? 'bot' : 'user'}'
+          />
+        </div>
+        <div class='message' id=${uniqueid}>${vlaue}</div>
+      </div>
+    </div>
+`
+
+  )
+}
+
+
+const handlesubmit = async (e) => {
+  e.preventDefault ();
+
+  const data = new FormData(form);
+
+  // users chatstripe
+
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  form.reset();
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+
+  loader(messageDiv);
+}
+
+// submit key for form 
+
+
+form.addEventListener('submit', handlesubmit);
+
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handlesubmit(e)
+  }
+})
