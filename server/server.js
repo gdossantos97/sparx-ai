6e0f5c72b373a6fc9bd2c12e1848450bed5288e3
Gabriel <gabe.cs1997@gmail.com>
2023-02-12 00:39:03 -0500

    open ai server bug fix

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import { Configuration, Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 
 
 dotenv.config();
 
-const Configuration = new Configuration({
+const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
