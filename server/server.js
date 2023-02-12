a4729c37a541357b01093e7df19c72bcc59601f7
Gabriel <gabe.cs1997@gmail.com>
2023-02-12 00:34:45 -0500

    open ai config

--- server/server.js
+++ server/server.js
@@ -24,5 +24,27 @@ app.get('/', async (req,res) => {
 });
 
 app.post('/', async (req, res) => {
-    
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.7,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        })
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).send({ error })
+    }
 })
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
